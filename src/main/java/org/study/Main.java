823f9ae80cc14a941c396a5564ef83a55ea56c33
AbanoubSamuel <abanoub.samuel@hotmail.com>
2024-08-18 14:56:36 +0300

    feat: added MapDemo

--- src/main/java/org/study/Main.java
+++ src/main/java/org/study/Main.java
@@ -1,47 +1,55 @@
 package org.study;
 
 import org.study.collections.*;
+import org.study.core.*;
 import org.study.generics.*;
+import org.study.oop.Employee;
+import org.study.oop.Human;
+import org.study.oop.dependancyInjection.*;
+import org.study.oop.inheritance.*;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
+import java.text.NumberFormat;
 import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-//        Class<MyClass> myClass = MyClass.class;
-//
-//        // Class level annotation
-//        if (myClass.isAnnotationPresent(MyAnnotation.class)) {
-//            MyAnnotation classAnnotation = myClass.getAnnotation(MyAnnotation.class);
-//            System.out.println("Class Annotation: " + classAnnotation.value());
-//        }
-//
-//        // Method level annotation
-//        for (Method method : myClass.getDeclaredMethods()) {
-//            if (method.isAnnotationPresent(CustomAnnotation.class)) {
-//                CustomAnnotation methodAnnotation = method.getAnnotation(CustomAnnotation.class);
-//                System.out.println("Method Annotation: " + methodAnnotation.value());
-//            }
-//        }
-//
-//        // Constructor level annotation
-//        for (Constructor<?> constructor : myClass.getDeclaredConstructors()) {
-//            if (constructor.isAnnotationPresent(CustomAnnotation.class)) {
-//                CustomAnnotation constructorAnnotation = constructor.getAnnotation(CustomAnnotation.class);
-//                System.out.println("Constructor Annotation: " + constructorAnnotation.value());
-//            }
-//        }
-//
-//        // Member level annotation
-//        for (Field field : myClass.getDeclaredFields()) {
-//            if (field.isAnnotationPresent(CustomAnnotation.class)) {
-//                CustomAnnotation fieldAnnotation = field.getAnnotation(CustomAnnotation.class);
-//                System.out.println("Field Annotation: " + fieldAnnotation.value());
-//            }
-//        }
-//
-//        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        ////////// CustomAnnotation /////////////
+        Class<MyClass> myClass = MyClass.class;
+
+        // Class level annotation
+        if (myClass.isAnnotationPresent(MyAnnotation.class)) {
+            MyAnnotation classAnnotation = myClass.getAnnotation(MyAnnotation.class);
+            System.out.println("Class Annotation: " + classAnnotation.value());
+        }
+
+        // Method level annotation
+        for (Method method : myClass.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(MyAnnotation.class)) {
+                MyAnnotation methodAnnotation = method.getAnnotation(MyAnnotation.class);
+                System.out.println("Method Annotation: " + methodAnnotation.value());
+            }
+        }
+
+        // Constructor level annotation
+        for (Constructor<?> constructor : myClass.getDeclaredConstructors()) {
+            if (constructor.isAnnotationPresent(MyAnnotation.class)) {
+                MyAnnotation constructorAnnotation = constructor.getAnnotation(MyAnnotation.class);
+                System.out.println("Constructor Annotation: " + constructorAnnotation.value());
+            }
+        }
+
+        // Member level annotation
+        for (Field field : myClass.getDeclaredFields()) {
+            if (field.isAnnotationPresent(MyAnnotation.class)) {
+                MyAnnotation fieldAnnotation = field.getAnnotation(MyAnnotation.class);
+                System.out.println("Field Annotation: " + fieldAnnotation.value());
+            }
+        }
+        ////////////// Stream API //////////////
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 //
 //        IntStream.of(numbers).forEach(number -> System.out.println("value: " + number));
 //
@@ -80,7 +88,7 @@ public class Main {
 //        Human human = new Human();
 //        System.out.println(human);
 //        human.setName("Abanoub");
-
+//
 //        int[] plants = new int[5];
 //        plants[0] = 1;
 //        plants[1] = 2;
@@ -111,39 +119,39 @@ public class Main {
 //        int income = 120_000;
 //        boolean hasHighIncome = (income > 100_000);
 //        System.out.println(hasHighIncome);
+        ////////////// Reflection API ////////////////
+        try {
+            // Load the Person class
+            Class<?> personClass = Class.forName("org.study.core.Person");
 
-//        try {
-//            // Load the Person class
-//            Class<?> personClass = Class.forName("org.study.core.Person");
-//
-//            // Create an instance of Person using the default constructor
-//            Object personInstance = personClass.getDeclaredConstructor()
-//                                               .newInstance();
-//
-//            // Access the 'name' field and set its value
-//            Field nameField = personClass.getDeclaredField("name");
-//            nameField.setAccessible(true); // Allows access to private fields
-//            nameField.set(personInstance, "John");
-//
-//            // Access the 'age' field and set its value
-//            Field ageField = personClass.getDeclaredField("age");
-//            ageField.setAccessible(true);
-//            ageField.set(personInstance, 30);
-//
-//            // Invoke the 'sayHello' method
-//            Method sayHelloMethod = personClass.getDeclaredMethod("sayHello");
-//            sayHelloMethod.invoke(personInstance);
-//
-//            // Print the values using getters
-//            Method getNameMethod = personClass.getDeclaredMethod("getName");
-//            Method getAgeMethod = personClass.getDeclaredMethod("getAge");
-//            System.out.println("Name: " + getNameMethod.invoke(personInstance));
-//            System.out.println("Age: " + getAgeMethod.invoke(personInstance));
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
+            // Create an instance of Person using the default constructor
+            Object personInstance = personClass.getDeclaredConstructor()
+                                               .newInstance();
 
-//        System.out.println("======== OOP =========");
+            // Access the 'name' field and set its value
+            Field nameField = personClass.getDeclaredField("name");
+            nameField.setAccessible(true); // Allows access to private fields
+            nameField.set(personInstance, "John");
+
+            // Access the 'age' field and set its value
+            Field ageField = personClass.getDeclaredField("age");
+            ageField.setAccessible(true);
+            ageField.set(personInstance, 30);
+
+            // Invoke the 'sayHello' method
+            Method sayHelloMethod = personClass.getDeclaredMethod("sayHello");
+            sayHelloMethod.invoke(personInstance);
+
+            // Print the values using getters
+            Method getNameMethod = personClass.getDeclaredMethod("getName");
+            Method getAgeMethod = personClass.getDeclaredMethod("getAge");
+            System.out.println("Name: " + getNameMethod.invoke(personInstance));
+            System.out.println("Age: " + getAgeMethod.invoke(personInstance));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        ////////////////////////// OOP /////////////////////////
+        System.out.println("======== OOP =========");
 //        TextBox textBox1 = new TextBox();
 //        TextBox textBox2 = textBox1;
 //        textBox1.setText("Box 1");
@@ -162,7 +170,7 @@ public class Main {
 //        for (UIControl control : controls) {
 //            control.render();
 //        }
-
+//
 //        TaxCalculator taxCalculator = new TaxCalculator2020();
 //        taxCalculator.calculateTax();
 //        TaxReport taxReport = new TaxReport(taxCalculator);
@@ -203,7 +211,7 @@ public class Main {
 //        for (Integer item : ints) {
 //            System.out.println(item);
 //        }
-
+        /////////////// Collection & List Interfaces //////////////
         System.out.println("========== Collection Demo =========");
         System.out.println("========== Collection Demo =========");
         CollectionDemo.show();
@@ -213,11 +221,40 @@ public class Main {
         System.out.println("========== Comparable Interface =========");
         System.out.println("========== Comparable Interface =========");
         List<Customer> customers = new ArrayList<>();
-        customers.add(new Customer("B", "e3"));
-        customers.add(new Customer("A", "e2"));
-        customers.add(new Customer("C", "e1"));
+        customers.add(new Customer("B", "e4"));
+        customers.add(new Customer("A", "e3"));
+        customers.add(new Customer("C", "e2"));
+        customers.add(new Customer("D", "e1"));
         System.out.println(customers);
-        Collections.sort(customers, new EmailComparator());
+        customers.sort(new EmailComparator());
         System.out.println(customers);
+        ////////////// Queue Interface //////////////////
+        Queue<String> queue = new ArrayDeque<>();
+        queue.add("B");
+        queue.add("C");
+        queue.add("A");
+        String peeked = queue.peek();
+        String element = queue.element();
+        System.out.println("Front element: " + peeked);
+        queue.remove();
+        System.out.println("Queue : " + queue);
+        ////////////// Set Interface //////////////////
+        Set<String> set = new HashSet<>();
+        set.add("B");
+        set.add("C");
+        set.add("D");
+        set.add("D");
+        System.out.println(set);
+        Collection<String> collection = new ArrayList<>();
+        Collections.addAll(collection, "A", "B", "C", "C");
+        Set<String> set1 = new HashSet<>(collection);
+        System.out.println(set1);
+        System.out.println("============ Combining Sets =============");
+        Set<String> set2 = new HashSet<>(Arrays.asList("A", "B", "C"));
+        Set<String> set3 = new HashSet<>(Arrays.asList("C", "D", "F"));
+        set2.addAll(set3);
+        System.out.println(set2);
+        ////////////// Map Interface ////////////////
+        MapDemo.show();
     }
 }
